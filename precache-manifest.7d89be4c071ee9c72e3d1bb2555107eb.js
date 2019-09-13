ec8b633100931c6a7071cf5789775c915a07ba10
cramos <chloieeeramos@gmail.com>
2019-09-13 12:03:13 -0400

    Updates

---
+++ precache-manifest.7d89be4c071ee9c72e3d1bb2555107eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51b3fb400839e55b0893e98ab749d57",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "95e6aeddafd1caf8b403",
+    "url": "/reactapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0b43feb5f43e1e8aeb51",
+    "url": "/reactapp/static/js/2.1f4aa7a3.chunk.js"
+  },
+  {
+    "revision": "95e6aeddafd1caf8b403",
+    "url": "/reactapp/static/js/main.bd1ff0a2.chunk.js"
+  },
+  {
+    "revision": "35de96586b575d93b647",
+    "url": "/reactapp/static/js/runtime~main.f743b627.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
